90df78a5cce36b80087960f447342e7e46905ea8
Anuj Arora <anujarora0502@gmail.com>
2025-12-01 03:44:01 +0530

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "nprogress/nprogress.css";
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </main>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
